63c9a6b7bf9acf6e4d40d06e47ab9a73e43ceb8c
Jonah Kirangi <jonahkirangi@gmail.com>
2014-03-11 16:26:57 -0700

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,18 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-casper');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-express-server');
+
 
   grunt.initConfig({
     simplemocha:{
       dev:{
-        src:['test/**/*_test.js'],
+        src:['test/*_test.js'],
         options:{
           reporter: 'spec',
           slow: 200,
@@ -31,10 +38,101 @@ module.exports = function(grunt) {
           module: true
         }
       }
+    },
+    casper: {
+      acceptance : {
+        options : {
+          test : true
+        },
+        files : {
+          'test/acceptance/casper-results.xml' : ['test/acceptance/*_test.js']
+        }
+      }
+    },
+    express: {
+      options: {
+        // Override defaults here
+      },
+      dev: {
+        options: {
+          script: 'app.js'
+        }
+      },
+      prod: {
+        options: {
+          script: 'app.js',
+          node_env: 'production'
+        }
+      },
+      test: {
+        options: {
+          script: 'app.js'
+        }
+      }
+    },
+    browserify: {
+      prod: {
+        src: ['assets/js/*.js'],
+        dest: 'dist/browser.js',
+        options: {
+          transform: ['debowerify'],
+          debug: false
+        }
+      },
+      dev: {
+        src: ['assets/js/*.js'],
+        dest: 'build/browser.js',
+        options: {
+          transform: ['debowerify'],
+          debug: true
+        }
+      }
+    },
+    clean: {
+      build: ['build'],
+      dev: {
+        src: ['build/**/*']
+      },
+      prod: ['dist']
+    },
+
+    copy: {
+      all: {
+        expand: true,
+        cwd: 'assets',
+        src: ['css/*.css', '*.html', 'images/**/*' ],
+        dest: 'dist/',
+        flatten: true,
+        filter: 'isFile'
+      },
+      dev: {
+        expand: true,
+        cwd: 'assets',
+        src: ['css/*.css', '*.html', 'images/**/*' ],
+        dest: 'build/',
+        flatten: false,
+        filter: 'isFile'
+      }
+    },
+    sass: {
+      dist: {
+        files: {'build/css/styles.css': 'assets/scss/styles.scss'}
+      },
+      dev: {
+        options: {
+          includePaths: ['public/scss/'],
+          sourceComments: 'map'
+        },
+        files: {'build/css/styles.css': 'assets/scss/styles.scss'}
+      }
     }
   });
 
-  grunt.registerTask('test', 'simplemocha:dev');
+  grunt.registerTask('default', ['jshint', 'test','watch:express']);
+  grunt.registerTask('test', [ 'jshint', 'simplemocha:dev' ]);
   grunt.registerTask('watch', 'test, watch:all');
+  grunt.registerTask('test:acceptance',['express:dev','casper']);
+  grunt.registerTask('server', [ 'build:dev', 'express:dev','watch:express' ]);
+  grunt.registerTask('build:dev',  ['clean:dev', 'sass:dev', 'browserify:dev', 'jshint:all', 'copy:dev']);
 
 };
---
+++ routes/users.js
@@ -0,0 +1,28 @@
+'use-strict';
+
+var User = require('../models/User');
+
+exports.collection = function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  User.find({}, function(err, users){
+    if(err){
+      res.writeHead(500);
+      res.send({'error': err});
+    } else {
+      res.send(JSON.stringify(users));
+    }
+  });
+};
+
+exports.createUser = function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  var user = new User(req.body);
+  user.save(function(err, responseUser){
+    if(err){
+      res.writeHead(500);
+      res.send({'error': err});
+    } else {
+      res.send(responseUser);
+    }
+  });
+};
